8cd1697a23fd047d7b4cca83796d62adef001bbd
Mantas Trojanovskis <mantastrojak@gmail.com>
2025-03-07 12:47:39 +0200

    Interface for Purchase

--- Interface/IPurchaseService.cs
+++ Interface/IPurchaseService.cs
@@ -1,6 +1,19 @@
-﻿namespace FluentBlazor_Project.Interface
+﻿using FluentBlazor_Project.Data.Models;
+
+namespace FluentBlazor_Project.Interface
 {
     public interface IPurchaseService
     {
+        Task<List<Purchase>> RetrieveAllPurchases();
+
+        Task<List<Purchase>> RetrieveUsersPurchases(string UserId);
+
+        Task DeletePurchaseAsync(Guid purchaseId);
+
+        Task DeletePurchaseItemAsync(Guid purchaseId, Guid productId);
+
+        Task ChangePurchaseItemQuantityAsync(Guid purchaseId, Guid productId, int quantity);
+
+        Task CreateNewPurchase(string userId, Dictionary<Guid, int> ProductQuantityPair);
     }
 }
